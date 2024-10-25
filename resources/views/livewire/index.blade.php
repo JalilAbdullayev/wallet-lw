7c8001e0a76211915eb56eaafab42ac21bf37a78
Jalil <abdullacelil00@gmail.com>
2024-10-25 15:47:47 +0400

    feat: create homepage component

---
+++ resources/views/livewire/index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Attributes\Title;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    #[Title('Home')]
+    public function mount() {
+    }
+}; ?>
+
+<div>
+    //
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
-Route::view('/', 'welcome');
+Route::view('dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::view('profile', 'profile')->middleware(['auth'])->name('profile');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Volt::route('/', 'index');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
